be15e10040168cba1577e2b9e4c5790e713c1809
amnforum <113209109+amnforum@users.noreply.github.com>
2023-04-16 11:56:03 +0530

    Add files via upload

---
+++ yolo/EYE_MOD.py
@@ -0,0 +1,154 @@
+import cv2 as cv
+import mediapipe as mp
+import time
+import utils_eye, math
+import numpy as np
+
+
+
+# landmark detection function 
+def landmarksDetection(img, results_eye, draw=False):
+    img_height, img_width= img.shape[:2]
+    # list[(x,y), (x,y)....]
+    mesh_coord = [(int(point.x * img_width), int(point.y * img_height)) for point in results_eye.multi_face_landmarks[0].landmark]
+    if draw :
+        [cv.circle(img, p, 2, (0,255,0), -1) for p in mesh_coord]
+
+    # returning the list of tuples for each landmarks 
+    return mesh_coord
+
+# Euclaidean distance 
+def euclaideanDistance(point, point1):
+    x, y = point
+    x1, y1 = point1
+    distance = math.sqrt((x1 - x)**2 + (y1 - y)**2)
+    return distance
+
+# Blinking Ratio
+def blinkRatio(img, landmarks, right_indices, left_indices):
+    # Right eyes 
+    # horizontal line 
+    rh_right = landmarks[right_indices[0]]
+    rh_left = landmarks[right_indices[8]]
+    # vertical line 
+    rv_top = landmarks[right_indices[12]]
+    rv_bottom = landmarks[right_indices[4]]
+    # draw lines on right eyes 
+    # cv.line(img, rh_right, rh_left, utils.GREEN, 2)
+    # cv.line(img, rv_top, rv_bottom, utils.WHITE, 2)
+
+    # LEFT_EYE 
+    # horizontal line 
+    lh_right = landmarks[left_indices[0]]
+    lh_left = landmarks[left_indices[8]]
+
+    # vertical line 
+    lv_top = landmarks[left_indices[12]]
+    lv_bottom = landmarks[left_indices[4]]
+
+    rhDistance = euclaideanDistance(rh_right, rh_left)
+    rvDistance = euclaideanDistance(rv_top, rv_bottom)
+
+    lvDistance = euclaideanDistance(lv_top, lv_bottom)
+    lhDistance = euclaideanDistance(lh_right, lh_left)
+
+    reRatio = rhDistance/rvDistance
+    leRatio = lhDistance/lvDistance
+
+    ratio = (reRatio+leRatio)/2
+    return ratio 
+
+# Eyes Extrctor function,
+def eyesExtractor(img, right_eye_coords, left_eye_coords):
+    # converting color image to  scale image 
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
+    # getting the dimension of image 
+    dim = gray.shape
+
+    # creating mask from gray scale dim
+    mask = np.zeros(dim, dtype=np.uint8)
+
+    # drawing Eyes Shape on mask with white color 
+    cv.fillPoly(mask, [np.array(right_eye_coords, dtype=np.int32)], 255)
+    cv.fillPoly(mask, [np.array(left_eye_coords, dtype=np.int32)], 255)
+
+    # showing the mask 
+    # cv.imshow('mask', mask)
+    
+    # draw eyes image on mask, where white shape is 
+    eyes = cv.bitwise_and(gray, gray, mask=mask)
+    # change black color to gray other than eys 
+    # cv.imshow('eyes draw', eyes)
+    eyes[mask==0]=155
+    
+    # getting minium and maximum x and y  for right and left eyes 
+    # For Right Eye 
+    r_max_x = (max(right_eye_coords, key=lambda item: item[0]))[0]
+    r_min_x = (min(right_eye_coords, key=lambda item: item[0]))[0]
+    r_max_y = (max(right_eye_coords, key=lambda item : item[1]))[1]
+    r_min_y = (min(right_eye_coords, key=lambda item: item[1]))[1]
+
+    # For LEFT Eye
+    l_max_x = (max(left_eye_coords, key=lambda item: item[0]))[0]
+    l_min_x = (min(left_eye_coords, key=lambda item: item[0]))[0]
+    l_max_y = (max(left_eye_coords, key=lambda item : item[1]))[1]
+    l_min_y = (min(left_eye_coords, key=lambda item: item[1]))[1]
+
+    # croping the eyes from mask 
+    cropped_right = eyes[r_min_y: r_max_y, r_min_x: r_max_x]
+    cropped_left = eyes[l_min_y: l_max_y, l_min_x: l_max_x]
+
+    # returning the cropped eyes 
+    return cropped_right, cropped_left
+
+# Eyes Postion Estimator 
+def positionEstimator(cropped_eye):
+    # getting height and width of eye 
+    h, w =cropped_eye.shape
+    
+    # remove the noise from images
+    gaussain_blur = cv.GaussianBlur(cropped_eye, (9,9),0)
+    median_blur = cv.medianBlur(gaussain_blur, 3)
+
+    # applying thrsholding to convert binary_image
+    ret, threshed_eye = cv.threshold(median_blur, 130, 255, cv.THRESH_BINARY)
+
+    # create fixd part for eye with 
+    piece = int(w/3) 
+
+    # slicing the eyes into three parts 
+    right_piece = threshed_eye[0:h, 0:piece]
+    center_piece = threshed_eye[0:h, piece: piece+piece]
+    left_piece = threshed_eye[0:h, piece +piece:w]
+    
+    # calling pixel counter function
+    eye_position, color = pixelCounter(right_piece, center_piece, left_piece)
+
+    return eye_position, color 
+
+# creating pixel counter function 
+def pixelCounter(first_piece, second_piece, third_piece):
+    # counting black pixel in each part 
+    right_part = np.sum(first_piece==0)
+    center_part = np.sum(second_piece==0)
+    left_part = np.sum(third_piece==0)
+    # creating list of these values
+    eye_parts = [right_part, center_part, left_part]
+
+    # getting the index of max values in the list 
+    max_index = eye_parts.index(max(eye_parts))
+    pos_eye ='' 
+    if max_index==0:
+        pos_eye="RIGHT"
+        color=[utils_eye.BLACK, utils_eye.GREEN]
+    elif max_index==1:
+        pos_eye = 'CENTER'
+        color = [utils_eye.YELLOW, utils_eye.PINK]
+    elif max_index ==2:
+        pos_eye = 'LEFT'
+        color = [utils_eye.GRAY, utils_eye.YELLOW]
+    else:
+        pos_eye="Closed"
+        color = [utils_eye.GRAY, utils_eye.YELLOW]
+    return pos_eye, color
